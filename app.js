1151b45a747df78a1b2b39e84c340179e4b0e344
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-08-30 12:54:33 -0400

    set up ejs and ejs-layouts

---
+++ app.js
@@ -0,0 +1,34 @@
+// Imports
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts')
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Static Files
+app.use(express.static('public'))
+
+// Set Templating Engine
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+
+// Routes
+app.use('/', require('./routes/index'))
+
+// Ports
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
